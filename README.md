20c606d1d777ad874c522ff9212a3f86010f2464
Nate Costello <natecostello@gmail.com>
2024-04-24 21:51:04 -0400

    Deleted file logs and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-This repo contains some scripts and utilities that will go from a draw.io diagram to csv and back.
+## Summary
+This repo contains some scripts and utilities that will go from a draw.io diagram to csv and back.  It needs to be refactored a bit, but its functional.
+
+## Going from an excel CSV to Drawio
+```sh
+xl_to_drawio.sh [inputfile] [outputfile]
+```
+Note: within this script is a command line call to draw.io to the final step of the conversion.  You will need to update `drawio_path="/Applications/draw.io.app/Contents/MacOS/draw.io"`, within `csv_to_drawio.sh` to reflect the location of draw.io on your system for the pipeline to work.
+
+## Going from Drawio to and excel CSV
+```sh
+drawio_to_xl.sh [inputfile] [outputfile]
+```
+
+## Odds and Ends
+There is a little hardcoding of headers, and header ordering.  The code is flexible to take arbitrary headers.  You may want to edit csv_reorder_headers.py if to suit your needs.
+
+
+
+
--- drawio_to_xl.sh
+++ drawio_to_xl.sh
@@ -11,16 +11,13 @@ input_file="$1"
 output_file="$2"
 
 # Run the Python scripts in a pipeline, passing the output of each command directly to the next
-drawio_to_csv.py -i "$input_file" frontmatter.txt | tee data/0drawio_to_csv_flex.csv | \
-csv_strip_frontmatter.py | tee data/1csv_strip_frontmattery.csv | \
-csv_delete_height_width.py | tee data/2csv_delete_height_width.csv | \
-csv_rename_ids.py | tee data/3csv_rename_ids.csv | \
-csv_strip_xl_ids.py | tee data/4csv_strip_xl_ids.csv | \
-csv_rename_shapes.py | tee data/5csv_rename_shapes.csv | \
-csv_parse_decisions.py | tee data/6csv_parse_decisions.csv | \
-csv_insert_newlines.py | tee data/7csv_insert_newlines.csv | \
-csv_rename_headers.py | tee data/8csv_rename_headers.csv | \
+drawio_to_csv.py -i "$input_file" frontmatter.txt | \
+csv_strip_frontmatter.py | \
+csv_delete_height_width.py | \
+csv_rename_ids.py | \
+csv_strip_xl_ids.py | \
+csv_rename_shapes.py | \
+csv_parse_decisions.py | \
+csv_insert_newlines.py | \
+csv_rename_headers.py | \
 csv_reorder_headers.py > "$output_file"
-#csv_rename_headers.py > "$output_file"
-
-#csv_to_xl.py > "$output_file"
